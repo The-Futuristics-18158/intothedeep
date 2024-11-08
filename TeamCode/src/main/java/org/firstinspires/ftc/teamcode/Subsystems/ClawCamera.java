6a706c6519d85d75acaec4e0aae3d31006f6b03d
suzhenxzhong <suzhenxzhong@gmail.com>
2024-11-08 00:26:44 -0500

    adjust dashboard show

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ClawCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ClawCamera.java
@@ -53,47 +53,45 @@ public class ClawCamera extends SubsystemBase {
     // Method to initialize dashboard with default (null) values
     private void initializeDashboard() {
         // Set up a fixed number of slots on the dashboard for detected colors
-        for (int i = 0; i < 4; i++) { // Assuming a maximum of 10 color detections
+        for (int i = 0; i < 4; i++) { // Assuming a maximum of 4 color detections
             RobotContainer.DBTelemetry.addData("Detected Color " + (i + 1), "null");
-            RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " Bounding Box",
-                    "Top-left: (null, null), Bottom-right: (null, null)"
-            );
+            RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " Center", "(null, null)");
+            RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " Angle", "null");
+            RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " YBR", "[Y=null, B=null, R=null]");
         }
         // Update the telemetry to show the initial layout
         RobotContainer.DBTelemetry.update();
     }
 
+
     // Method to update the dashboard with actual detected values
-    public void updateDashboard() {
-        // Get the list of detected colors
-        List<DetectedColorWithAngle> detectedColors = GetCurrentColAndAng();
+    private void updateDashboard() {
+        // Assume detectedColors is a list of DetectedColorWithAngle objects containing the detected values
+        List<DetectedColorWithAngle> detectedColors = GetCurrentColAndAng(); // Replace with your actual method to get detected colors
 
-        // Update each slot with the detected values or leave them as null if no detection
-        for (int i = 0; i < 4; i++) { // Assuming a maximum of 10 color detections
+        for (int i = 0; i < 4; i++) {
             if (i < detectedColors.size()) {
-                DetectedColorWithAngle detected = detectedColors.get(i);
+                DetectedColorWithAngle detectedColor = detectedColors.get(i);
 
-                // Update detected color name and area in the telemetry
-                RobotContainer.DBTelemetry.addData("Detected Color " + (i + 1), detected.colorName);
-
-                // Update bounding box information
-                RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " Bounding Box",
-                        "Top-left: (" + detected.boundingBox.x + ", " + detected.boundingBox.y + "), " +
-                                "Bottom-right: (" + (detected.boundingBox.x + detected.boundingBox.width) + ", " +
-                                (detected.boundingBox.y + detected.boundingBox.height) + ")"
-                );
+                // Update the dashboard with the actual detected values
+                RobotContainer.DBTelemetry.addData("Detected Color " + (i + 1), detectedColor.getColor());
+                RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " Center",
+                        "(" + detectedColor.getCenter().x + ", " + detectedColor.getCenter().y + ")");
+                RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " Angle", detectedColor.getAngle());
+                RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " YBR",
+                        "[Y=" + detectedColor.getColor()[0] + ", B=" + detectedColor.getColor()[1] + ", R=" + detectedColor.getColor()[2] + "]");
             } else {
-                // No detection for this slot, set it to null or empty string
+                // If there are fewer detections than slots, fill remaining slots with "null"
                 RobotContainer.DBTelemetry.addData("Detected Color " + (i + 1), "null");
-                RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " Bounding Box",
-                        "Top-left: (null, null), Bottom-right: (null, null)"
-                );
+                RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " Center", "(null, null)");
+                RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " Angle", "null");
+                RobotContainer.DBTelemetry.addData("Color " + (i + 1) + " YBR", "[Y=null, B=null, R=null]");
             }
         }
-
-        // Update the telemetry to reflect the latest data on the dashboard
+        // Update the telemetry to reflect new data
         RobotContainer.DBTelemetry.update();
     }
+
 
     // get current AprilTag detections (if any) from camera
     // returns list containing info on each tag detected
