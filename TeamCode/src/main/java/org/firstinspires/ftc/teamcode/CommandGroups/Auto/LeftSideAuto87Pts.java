8083bd12c88e2071e760beecddee2c095d24e48b
superzokabear <superzokabear@gmail.com>
2025-02-06 21:04:57 -0500

    using buttion in auto and merging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/Auto/LeftSideAuto87Pts.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/Auto/LeftSideAuto87Pts.java
@@ -6,6 +6,7 @@ import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.geometry.Translation2d;
 
+import org.firstinspires.ftc.teamcode.CommandGroups.ArmPositions.ArmStowHigh;
 import org.firstinspires.ftc.teamcode.CommandGroups.AutoRecalibrateOdometry;
 import org.firstinspires.ftc.teamcode.CommandGroups.AutomatedMovements.HighBucketDeposit;
 import org.firstinspires.ftc.teamcode.CommandGroups.AutomatedMovements.GroundCyclingAuto;
@@ -32,44 +33,46 @@ public class LeftSideAuto87Pts extends SequentialCommandGroup {
         addCommands (
                 // sets the starting position
                 new InstantCommand(() -> RobotContainer.odometry.setCurrentPos(AutoFunctions.redVsBlue(new Pose2d(0.8, 1.57, new Rotation2d(Math.toRadians(-90)))))),
-//
-//                // makes sure the claw is closed
-//                new CloseClaw(),
-//
-//                // put arm into stow position
-//                // powers shoulder
-//                new InstantCommand(() ->RobotContainer.shoulderJoint.RotateTo(122)),
-//                // folds the elbow in 225
-//                new InstantCommand(() ->RobotContainer.elbowJoint.RotateTo(270)),
-//                // folds the wrist in 45
-//                new InstantCommand(() -> RobotContainer.flappyFlappyWrist.RotateTo(0)),
-//
-//                new InstantCommand(() -> RobotContainer.wristRotateServo.RotateTo(180)),
-//
-//                // move slide up to get ready for drop off
-//                new InstantCommand(()-> RobotContainer.linearSlide.moveTo(SlideTargetHeight.SAMPLE_HIGH)),
-//
-//
-//                // drop off first sample into bucket
-//                new HighBucketDeposit(),
-//
-//                // pickup all three and cycling in high bucket
-//                new GroundCyclingAuto(),
-//
-//
-//                new InstantCommand(() ->RobotContainer.shoulderJoint.RotateTo(132)),
-//
-//                // folds the elbow in 270
-//                new InstantCommand(() ->RobotContainer.elbowJoint.RotateTo(250)),
-//
-//                // folds the wrist in 0
-//                new InstantCommand(() -> RobotContainer.flappyFlappyWrist.RotateTo(0)),
-//
-//                // powers the wrist and moves it to straight position
-//                new InstantCommand(() -> RobotContainer.wristRotateServo.RotateTo(180)),
-//
-//                new OpenClaw(),
-//                // move to park position
+
+                // makes sure the claw is closed
+                new CloseClaw(),
+
+                // put arm into stow position
+                // powers shoulder
+                new InstantCommand(() ->RobotContainer.shoulderJoint.RotateTo(122)),
+                // folds the elbow in 225
+                new InstantCommand(() ->RobotContainer.elbowJoint.RotateTo(270)),
+                // folds the wrist in 45
+                new InstantCommand(() -> RobotContainer.flappyFlappyWrist.RotateTo(0)),
+
+                new InstantCommand(() -> RobotContainer.wristRotateServo.RotateTo(180)),
+
+                // move slide up to get ready for drop off
+                new InstantCommand(()-> RobotContainer.linearSlide.moveTo(SlideTargetHeight.SAMPLE_HIGH)),
+
+
+                // drop off first sample into bucket
+                new HighBucketDeposit(),
+
+                // pickup all three and cycling in high bucket
+                new GroundCyclingAuto(),
+
+
+                new InstantCommand(() ->RobotContainer.shoulderJoint.RotateTo(132)),
+
+                // folds the elbow in 270
+                new InstantCommand(() ->RobotContainer.elbowJoint.RotateTo(250)),
+
+                // folds the wrist in 0
+                new InstantCommand(() -> RobotContainer.flappyFlappyWrist.RotateTo(0)),
+
+                // powers the wrist and moves it to straight position
+                new InstantCommand(() -> RobotContainer.wristRotateServo.RotateTo(180)),
+
+                new OpenClaw(),
+                // move to park position
+
+
 
                 new FollowPath(
                         2.0,
