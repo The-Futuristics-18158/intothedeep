a2d6937d132159951fcab35abea29101528f3542
superzokabear <superzokabear@gmail.com>
2025-02-17 20:32:56 -0500

    fixing climb angle it needs a little more and then it should be good

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/ArmPositions/SpecimenPlacePos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/ArmPositions/SpecimenPlacePos.java
@@ -20,10 +20,10 @@ public class SpecimenPlacePos extends SequentialCommandGroup {
 
                 // lifts the shoulder up 90+-60 degrees
                 // lifts the shoulder up to 135 degrees
-                new InstantCommand(() ->RobotContainer.shoulderJoint.RotateTo(135)),
+                new InstantCommand(() ->RobotContainer.shoulderJoint.RotateTo(145)),
 
                 // folds the elbow in 270
-                new InstantCommand(() ->RobotContainer.elbowJoint.RotateTo(275)),
+                new InstantCommand(() ->RobotContainer.elbowJoint.RotateTo(270)),
 
                 // folds the wrist in 0
                 new InstantCommand(() -> RobotContainer.flappyFlappyWrist.RotateTo(0)),
